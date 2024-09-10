3ebc7cc9c684b224acfc59c97d6e79e3e6c17356
Gonzalo <gonxaraya991@gmail.com>
2024-09-10 19:34:20 -0300

    drag gesture

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -12,22 +12,46 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
             }
         });
 
-    const imageStyle = useAnimatedStyle(() => {
-        return {
-            width: withSpring(scaleImage.value),
-            height: withSpring(scaleImage.value),
-        };
-    })
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+    const drag = Gesture.Pan().onChange((event) => {
+        translateX.value += event.changeX;
+        translateY.value += event.changeY;
+    });
+
 
     return (
-        <View style={{ top: -350 }} >
-            <GestureDetector gesture={doubleTap}>
-                <Animated.Image
-                    source={stickerSource}
-                    resizeMode="contain"
-                    style={[imageStyle, { width: imageSize, height: imageSize }]}
-                />
-            </GestureDetector>
-        </View>
+        <GestureDetector gesture={drag}>
+            <Animated.View style={{ top: -350 }} >
+                <GestureDetector gesture={doubleTap}>
+                    <Animated.Image
+                        source={stickerSource}
+                        resizeMode="contain"
+                        style={[imageStyle, { width: imageSize, height: imageSize }]}
+                    />
+                </GestureDetector>
+            </Animated.View>
+        </GestureDetector>
     );
 }
+
+const imageStyle = useAnimatedStyle(() => {
+    return {
+        width: withSpring(scaleImage.value),
+        height: withSpring(scaleImage.value),
+    };
+});
+
+const containerStyle = useAnimatedStyle(() => {
+    return {
+        transform: [
+            {
+                translateX: translateX.value,
+            },
+            {
+                translateY: translateY.value,
+            },
+        ],
+    };
+});
+
