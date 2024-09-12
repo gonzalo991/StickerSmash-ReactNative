659366a123e5f9744b25173132fada8fb0551bbd
Gonzalo <gonxaraya991@gmail.com>
2024-09-12 19:34:27 -0300

    configuraciones finales

--- App.js
+++ App.js
@@ -116,7 +116,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   )
 }
